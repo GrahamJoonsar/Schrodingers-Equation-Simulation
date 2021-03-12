01465f181b5a2b241c78cdc251c175bd9b809247
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-03-11 22:01:53 -0500

    Update sEquation.py

--- sEquation.py
+++ sEquation.py
@@ -18,7 +18,26 @@ class Particle:
     def draw(self):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
+class Wall:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+    
+    def draw(self):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+
+# The Particle
 particle = Particle(250, 250, 10, (120, 60, 245))
+
+# Walls
+bottom = Wall(100, 480, 300, 20, (100, 100, 100))
+left = Wall(100, 0, 20, 500, (100, 100, 100))
+right = Wall(380, 0, 20, 500, (100, 100, 100))
+walls = [bottom, left, right]
 
 running = True
 while running:
@@ -31,5 +50,7 @@ while running:
     win.fill((0, 0, 0))
 
     particle.draw()
+    for wall in walls:
+        wall.draw()
 
     pygame.display.update()
